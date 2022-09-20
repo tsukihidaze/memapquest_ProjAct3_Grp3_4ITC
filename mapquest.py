fdd1092c0777fd8d2e8c7b219feb101323642686
toratoratora-code <68171064+toratoratora-code@users.noreply.github.com>
2022-09-20 15:11:13 +0800

    Adjust: input check

--- mapquest.py
+++ mapquest.py
@@ -1,7 +1,6 @@
-from ast import And
 import urllib.parse
 import requests
-from colorama import Fore
+from colorama import Fore #colorama for print() color options
 from colorama import Style
 from texttable import Texttable
 
@@ -13,11 +12,12 @@ t = Texttable()
 def printDir():
         if "Welcome" not in each["narrative"] and myUnits == "1":
             print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])) + " miles)") + "\n")
-        if "Welcome" not in each["narrative"] and myUnits == "2":
+        if "Welcome" not in each["narrative"] and myUnits == "2" or myUnits.isalpha():
             print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)") + "\n")
 
+
         if "Welcome" in each["narrative"]:
-            print(f"{Style.BRIGHT}{Fore.GREEN}Finally! {Style.RESET_ALL}" + (each["narrative"]) + "\n")
+            print(f"{Style.BRIGHT}{Fore.GREEN}\n\nFinally! {Style.RESET_ALL}" + (each["narrative"]) + "\n")
 
         # print(f"This is {Fore.GREEN}color{Style.RESET_ALL}!")
 def printCardinalDir():
@@ -27,19 +27,20 @@ def printCardinalDir():
             print(f"{Style.BRIGHT}{Fore.GREEN}Welcome to your Destination {Style.RESET_ALL}{Style.BRIGHT}{Fore.BLUE}" + name + f"!{Style.RESET_ALL}")
 
 # main
+# prints "Welcome messages"
 print(f"{Style.BRIGHT}----------------------------------{Style.RESET_ALL}")
 print (f"{Style.BRIGHT}| Welcome to Group 4's {Fore.CYAN}MapQuest!{Style.RESET_ALL}{Style.BRIGHT} |{Style.RESET_ALL}")
 print(f"{Style.BRIGHT}----------------------------------{Style.RESET_ALL}")
 
 print (f"Remember to enter 'q' or 'quit' to {Style.BRIGHT}{Fore.RED}exit{Style.RESET_ALL} the program!\n")
-
+# Start of user input
 name = input("Enter your name: ")
 if name == "quit" or name == "q":
         exit()
-
+#While loop that continuously seeks user input
 while True:
     print (f"\nRemember to enter 'q' or 'quit' to {Style.BRIGHT}{Fore.RED}exit{Style.RESET_ALL} the program!\n")
-    myUnits = input("Type 1 if you prefer Miles\nType 2 if you prefer Kilometers: ")
+    myUnits = input("Unit Measurement - 1 (Miles), 2 (Kilometers): ")
     if myUnits == "quit" or myUnits == "q":
         break
     orig = input("Starting Location: ")
@@ -57,25 +58,27 @@ while True:
     print(f"{Style.BRIGHT}\n\nURL to your directions: {Style.RESET_ALL}" + (url))
 
     json_data = requests.get(url).json()
-
+    
+    #if Input is correct, retrieve status code.
     json_status = json_data["info"]["statuscode"]
 
     if json_status == 0:
 
-        print("=============================================")
+        print(f"{Style.BRIGHT}============================================={Style.RESET_ALL}")
 
         print("DIRECTIONS FROM " + (orig.upper()) + " to " + (dest.upper()))
 
         print("Trip Duration:   " + (json_data["route"]["formattedTime"]))
 
-        if myUnits == "1":
+        if myUnits == "1": #Simple check for distance units
             print("Miles:      " + str("{:.2f}".format((json_data["route"]["distance"]))))
         if myUnits == "2":
             print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
-
+        if myUnits != "1" or "2":
+            print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
         print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
 
-        print("=============================================")
+        print(f"{Style.BRIGHT}============================================={Style.RESET_ALL}")
         for each in json_data["route"]["legs"][0]["maneuvers"]:
             printDir()
             printCardinalDir()
